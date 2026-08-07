7db954d6738563fd38d790c1eaa2b08ca7424a04
princ <princ@AUTUMNLEAF>
2026-08-07 10:10:56 +0200

    Day 2 pt.1 - correct solution

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-points = 0
-startPos=50
-maxPos=99
-minPos=0
-
-print("startPos:", startPos)
-print("maxPos:", maxPos)
 file = open("adventofcode_secretentrance.txt", "r")
-for l in file:
-    print("Start Position:" + str(startPos))
-    direction = l[0]
-    print("direction: " + direction)
-    count = l[1:]
-    print("count: " + count)
 
-    num = int(count)
-    if direction == "L":
-        for i in range(num):
-            startPos = startPos - 1
-            if startPos == -1:
-                startPos = maxPos
-            if startPos == 0:
-                print("Zero here")
-                points += 1
-    else:
-        for i in range(num):
-            startPos = startPos + 1
-            if startPos == 100:
-                startPos = minPos
-            if startPos == 0:
-                print("Zero here")
-                points += 1
+total = 0
 
-print("Points:", points)
+numList = file.readlines()
+for l in numList:
+    print (l)
+    idList = l.split(",")
+    print (idList)
+    for id in idList:
+        individ = id.split("-")
+        print (individ)
+        num1 = int(individ[0])
+        num2 = int(individ[1])
+        counter = num1 -1
+        idRange = num2 - num1
+        print (idRange)
+        for i in range(idRange):
+            counter += 1
+            count = len(str(counter))
+            if count % 2 != 0:
+                continue
+            else:
+                temp = str(counter)
+                q, r = divmod(len(temp), 2)
+                part1, part2 = temp[:q + r], temp[q + r:]
+                if part1 == part2:
+                    total += int(counter)
+
+print (total)
