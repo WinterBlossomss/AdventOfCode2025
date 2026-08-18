6d45097cfd2622fb454d7112591de7d7aff920c1
princ <princ@AUTUMNLEAF>
2026-08-18 14:58:52 +0200

    Day 3 pt.2 - correct solution

--- main.py
+++ main.py
@@ -1,52 +1,26 @@
 file = open("adventofcode_secretentrance.txt", "r")
-
 total = 0
-
-def getbattery(num):
-    s = str(num)
-    n = len(s)
-    for d in range(1, n):
-        if n % d == 0 and s[:d] * (n // d) == s:
-            return True
-    return False
-
 numList = file.readlines()
 for l in numList:
-    bank = list(l)
-    sortmax2=0
-    jolt = 0
-    x = 0
-    max1 = 0
-    max2 = 0
-    pos1 = 0
-    pos2 = 0
-    max2,max1 = sorted(bank)[-2:]
-    pos1 = bank.index(max1)
-    pos2 = bank.index(max2)
-    # if higher number is first ( 9 1 )
-    if (pos1 < pos2):
-        jolt = int(str(max1)+str(max2))
-    #if higher number is second ( 1 9 )
-    else:
-        x = l[pos1:]
-        x = x[1:]
-        if x != '\n':
-            sortmax2 = sorted(x)[-1]
-            jolt = int(str(max1) + str(sortmax2))
-        else:
-            jolt = int(str(max2) + str(max1))
+    bank = l.strip()
 
+    removalcount = len(bank) - 12 # number of digits
+    stack = [] # highest number goes here by the digit
+    for digit in bank:
+        # checks if previous number is bigger than digit
+        # pop previously added digit if smaller than current digit (until no removals left) and append digit regardless
+        while stack and removalcount > 0 and stack[-1] < digit:
+            stack.pop()
+            removalcount -= 1
 
+        stack.append(digit)
+
+    jolt = int("".join(stack[:12]))
     total += jolt
-    print("bank = " + l)
-    print("x = " + str(x))
-    print("max1 = " + max1)
-    print("max2 = " + str(max2))
-    print("sortmax2 = " + str(sortmax2))
-    print("pos1 = " + str(pos1))
-    print("pos2 = " + str(pos2))
-    print("jolt = " + str(jolt))
-    print("total = " + str(total))
-    print("------------------")
 
 print(total)
+
+
+
+
+
