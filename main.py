94af02adedd686aa2bb862ac2126ac5dcbe60e38
princ <princ@AUTUMNLEAF>
2026-08-18 07:25:36 +0200

    Day 2 pt.2 - correct solution

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ file = open("adventofcode_secretentrance.txt", "r")
 
 total = 0
 
+def compare(num):
+    s = str(num)
+    n = len(s)
+    for d in range(1, n):
+        # n = total length of string
+        # d = block length (section you are checking for repeats)
+        # first check: does the block length divide evenly into total length
+        # second check: does repeating the block d*(n//d) times reproduce the original
+        if n % d == 0 and s[:d] * (n // d) == s:
+            return True
+    return False
+
+
 numList = file.readlines()
 for l in numList:
     print (l)
@@ -13,26 +26,12 @@ for l in numList:
         num1 = int(individ[0])
         num2 = int(individ[1])
         counter = num1 -1
-        idRange = num2 - num1
+        idRange = num2 - num1 + 1
         print (idRange)
         for i in range(idRange):
             counter += 1
-            count = len(str(counter))
-            if count % 2 != 0:
-                temp = list(map(int, str(counter)))
-                num = len(str(temp[0]))
-                # compare all list items
-                for i in range(num):
-                    for j in range(i + 1, num):
-                        compare(temp[i], temp[j])
-            else:
-                temp = str(counter)
-                q, r = divmod(len(temp), 2)
-                part1, part2 = temp[:q + r], temp[q + r:]
-                if part1 == part2:
-                    total += int(counter)
+            if compare(counter):
+                total += counter
 
 print (total)
 
-def compare(num1, num2):
-    if num1 == num2:
