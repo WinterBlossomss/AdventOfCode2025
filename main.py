82c892d8787478ea74825bee4f769822ad23c595
princ <princ@AUTUMNLEAF>
2026-08-19 15:11:06 +0200

    Day 6 WIP

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 file = open("adventofcode_secretentrance.txt", "r")
 
-total = 0
-ranges = []
-ids = []
+rows = file.readlines()
+numbers = rows.split(" ")
+calculations = []
 
-for row in file:
-    row = row.strip()
-    if not row: # skip stinky empty line dumb dumb empy line
-        continue
-    if "-" in row:
-        start, end = map(int, row.split("-"))
-        ranges.append((start, end))
-    else:
-        ids.append(int(row))
-
-ranges.sort()
-mergedranges = []
-
-for start, end in ranges:
-    # compare start with current range end - merge (take start and end to make new range)
-    if mergedranges and start <= mergedranges [-1][1] + 1:
-        mergedranges[-1] = (mergedranges[-1][0], max(mergedranges[-1][1], end))
-    else:
-        mergedranges.append((start, end))
-    total = sum(end - start + 1 for start, end in mergedranges)
-
-print(total)
-# print(ranges)
-# print(ids)
+print(numbers)
